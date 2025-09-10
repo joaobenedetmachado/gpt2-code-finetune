77e77512edf69142c9dbe53fe5ef0b46dca521f5
joao machado <jvbm2177@gmail.com>
2025-09-10 19:17:10 -0300

    added input in terminal and fixed model.generate output

--- src/generate.py
+++ src/generate.py
@@ -10,19 +10,24 @@ model = AutoModelForCausalLM.from_pretrained("gpt2")
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
-prompt = "def soma(a, b):"  # exemplo de inicio de código
-inputs = tokenizer(prompt, return_tensors="pt").to(device)
+while True:
+    prompt = input("digite o prompt para auto-complete: ")
 
-outputs = model.generate(
-    **inputs,
-    max_length=50,       # max saída | 1024tokens no gpt2
-    do_sample=True,      # True = respostas despadronizadas
-    temperature=1.1,     # criatividade do modelo
-    top_k=50,            # top_k para limitar diversidade
-    top_p=0.95,          # top_p para limitar diversidade
-    pad_token_id=tokenizer.eos_token_id  # evita erro de padding
-)
+    if prompt.lower() == exit:
+        break
 
-generated_code = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print("codigo gerado")
-print(generated_code)
+    inputs = tokenizer(prompt, return_tensors="pt").to(device)
+
+    outputs = model.generate(
+        **inputs,
+        max_length=50,
+        do_sample=True,
+        temperature=0.8,
+        top_k=50,
+        top_p=0.95,
+        pad_token_id=tokenizer.eos_token_id
+    )
+
+    generated_code = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    print("\n -- codigo gerado --")
+    print(generated_code)
