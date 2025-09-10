d6b01327bfee18d7b98e715fdabfc489eaf12718
joao machado <jvbm2177@gmail.com>
2025-09-10 19:08:28 -0300

    added tokenizer and pretreinad-model

--- src/generate.py
+++ src/generate.py
@@ -1,1 +1,7 @@
-# testar auto-complete
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# carrega o tokenizer
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
+
+# carrega o modelo pre treinado
+model = AutoModelForCausalLM.from_pretrained("gpt2")
